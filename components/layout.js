dcae60e5d8281095ef8e7ab5f3b355e69faf0d9f
oludchak <oleksandr.ludchak@devpulse.io>
2025-06-10 22:42:31 +0300

    minor changes

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import Link from 'next/link';
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 
-const name = 'Your name';
+const name = 'John Doe';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
